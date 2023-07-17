79e7d00660e81484bb3bcef3461b4d884aa48e50
TriMadon <mehammed6789@gmail.com>
2023-07-17 21:11:46 +0300

    Add hover effects

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const GRID_WIDTH = 720;
 const grid = document.querySelector(".grid");
+let sqaures = [];
 
-function createSqaure() {
+function createSqaure(width, color = "brown") {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = `${width}px`;
+    square.style.height = `${width}px`;
+    square.style.color = color;
     return square;
 }
 
@@ -13,11 +18,22 @@ function addSquare(squares) {
 }
 
 function createGrid(numSqaures) {
-    let sqaures = [];
+    let gaps = numSqaures - 1;
+    let sqWidth = (720 - gaps)/numSqaures; 
+    console.log(sqWidth);
     for (let i = 0; i < numSqaures**2; i++) {
-        sqaures.push(createSqaure());
+        sqaures.push(createSqaure(sqWidth, "brown"));
     }
     addSquare(sqaures);
 }
 
 createGrid(16);
+
+sqaures.forEach(square => {
+    square.addEventListener("mouseover", (e) => {
+        e.target.classList.add("colored", "hovered");
+    })
+    square.addEventListener("mouseout", (e) => {
+        e.target.classList.remove("hovered");
+    })
+})
