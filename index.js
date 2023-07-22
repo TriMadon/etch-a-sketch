c5e94dacfc636491851cfdbf47fdf2210f04b13c
TriMadon <mehammed6789@gmail.com>
2023-07-22 15:42:31 +0300

    Add brush darkening effect + clean up code

--- index.js
+++ index.js
@@ -1,108 +1,11 @@
+// #region constants
+
 const GRID_WIDTH = 720;
 const DEFAULT_COLOR = "brown";
 const DEFAULT_BRUSH_COLOR = "aquamarine";
 const DEFAULT_BRUSH_MODE = "solid";
+
 const grid = document.querySelector(".grid");
-
-let gridColor = DEFAULT_COLOR;
-let brushColor = DEFAULT_BRUSH_COLOR;
-let brushMode = DEFAULT_BRUSH_MODE;
-
-function createSqaure(width, color = gridColor) {
-	let square = document.createElement("div");
-	square.classList.add("square");
-	square.style.width = `${width}px`;
-	square.style.height = `${width}px`;
-	square.style.backgroundColor = color;
-	square.style.borderRadius = "15%";
-	square.addEventListener("mouseover", changeColor);
-	return square;
-}
-
-function changeColor(e) {
-	if (brushMode === "solid") {
-		e.target.style.backgroundColor = brushColor;
-	} else if (brushMode === "random") {
-		e.target.style.backgroundColor = generateRandomColor();
-	}
-}
-
-function generateRandomColor() {
-	return `RGB(${Math.floor(Math.random() * 255) + 1}, ${
-		Math.floor(Math.random() * 255) + 1
-	}, ${Math.floor(Math.random() * 255) + 1})`;
-}
-
-function addSquare(squares) {
-	squares.forEach((square) => {
-		grid.appendChild(square);
-	});
-}
-
-function getAllSquares() {
-	return document.querySelectorAll(".square");
-}
-
-function createGrid(numSqaures) {
-	let sqaures = [];
-	let gaps = numSqaures - 1;
-	let sqWidth = (720 - gaps) / numSqaures;
-	for (let i = 0; i < numSqaures ** 2; i++) {
-		let square = createSqaure(sqWidth);
-		if (numSqaures < 26) {
-			makeItPop(square);
-		}
-		if (numSqaures > 50) {
-			square.style.borderRadius = "";
-		}
-		sqaures.push(square);
-	}
-	addSquare(sqaures);
-}
-
-function makeItPop(square) {
-	square.addEventListener("mouseover", (e) => {
-		e.target.classList.add("hovered");
-	});
-	square.addEventListener("mouseout", (e) => {
-		e.target.classList.remove("hovered");
-	});
-}
-
-function clearGrid() {
-	getAllSquares().forEach((square) => {
-		square.style.backgroundColor = gridColor;
-	});
-}
-
-function deleteGrid() {
-	grid.replaceChildren();
-}
-
-function makeNewGrid(newSize) {
-	deleteGrid();
-	createGrid(newSize);
-}
-
-function promptUserForSize() {
-	let newSize = +prompt("Type the new size of the grid (1-100)");
-	if (
-		!Number.isInteger(newSize) ||
-		!newSize ||
-		newSize < 0 ||
-		newSize > 100
-	) {
-		return;
-	}
-	makeNewGrid(newSize);
-}
-
-function recolorGrid(color) {
-	gridColor = color;
-	clearGrid();
-}
-
-makeNewGrid(16);
 
 const allButtons = document.querySelectorAll("button");
 const resizeButton = document.getElementById("resize");
@@ -122,30 +25,200 @@ const recolorOk = document.querySelector("#colorModal .submit");
 const brushModal = document.getElementById("brushModal");
 const brushColorMode = document.getElementById("colorMode");
 const brushColorPicker = document.querySelector("#brushModal .colorPicker");
+const darkEffectCheckbox = document.querySelector("#brushModal .checkbox");
 const brushOk = document.querySelector("#brushModal .submit");
 
-resizeButton.onclick = () => resizeModal.showModal();
-resizeSlider.onmousemove = () => showSliderValue(resizeSlider.value);
-resizeOk.onclick = () => makeNewGrid(resizeSlider.value);
+// #endregion
 
-function showSliderValue(value) {
-	rangeValue.textContent = value;
+// #region variables
+
+let gridColor = DEFAULT_COLOR;
+let brushColor = DEFAULT_BRUSH_COLOR;
+let brushMode = DEFAULT_BRUSH_MODE;
+let darkeningEffect = false;
+let brightnessTrackerMap = new Map();
+
+// #endregion
+
+// #region BrightnessTracker Class
+
+class BrightnessTracker {
+	constructor(element) {
+		this.element = element;
+		this.brightness = 1;
+		this.visited = false;
+	}
+
+	resetBrightness() {
+		this.brightness = 1;
+		this.visited = false;
+		this.applyBrightness();
+	}
+
+	changeBrightnessBy(delta) {
+		if (!this.visited) {
+			this.visited = true;
+			return;
+		}
+		this.brightness = Math.max(0, this.brightness + delta);
+		this.applyBrightness();
+	}
+
+	applyBrightness() {
+		this.element.style.filter = `brightness(${this.brightness})`;
+	}
 }
 
-clearButton.onclick = () => clearGrid();
+// #endregion
 
-recolorButton.onclick = () => recolorModal.showModal();
-recolorOk.onclick = () => recolorGrid(recolorPicker.value);
+// #region reusable functions
 
-customizeBrushButton.onclick = () => brushModal.showModal();
-brushColorMode.addEventListener("change", function () {
-	if (brushColorMode.value === "solid") {
-		brushColorPicker.style.display = "block";
-	} else {
-		brushColorPicker.style.display = "none";
+function createSqaure(width, color = gridColor) {
+	let square = document.createElement("div");
+	square.classList.add("square");
+	square.style.width = `${width}px`;
+	square.style.height = `${width}px`;
+	square.style.backgroundColor = color;
+	square.style.borderRadius = "15%";
+	square.addEventListener("mouseover", changeColor);
+	return square;
+}
+
+function changeColor(e) {
+	if (brushMode === "solid") {
+		e.target.style.backgroundColor = brushColor;
+	} else if (brushMode === "random") {
+		e.target.style.backgroundColor = generateRandomColor();
 	}
-});
-brushOk.onclick = () => {
-	brushMode = brushColorMode.value? brushColorMode.value : DEFAULT_BRUSH_MODE;
-	brushColor = brushColorPicker.value;
+	if (darkeningEffect) {
+		brightnessTrackerMap.get(e.target).changeBrightnessBy(-0.1);
+	} else {
+		e.target.style.filter = "brightness(1)";
+	}
+}
+
+function generateRandomColor() {
+	return `RGB(${Math.floor(Math.random() * 255) + 1}, ${
+		Math.floor(Math.random() * 255) + 1
+	}, ${Math.floor(Math.random() * 255) + 1})`;
+}
+
+function addSquare(squares) {
+	squares.forEach((square) => {
+		grid.appendChild(square);
+	});
+}
+
+function getAllSquares() {
+	return document.querySelectorAll(".square");
+}
+
+function createGrid(numSquares) {
+	const MAX_SIZE_FOR_POP_EFFECT = 26;
+	const MAX_SIZE_FOR_ROUND_BORDER = 50;
+	let squares = [];
+	let gaps = numSquares - 1;
+	let sqWidth = (GRID_WIDTH - gaps) / numSquares;
+	for (let i = 0; i < numSquares ** 2; i++) {
+		let square = createSqaure(sqWidth);
+		if (numSquares < MAX_SIZE_FOR_POP_EFFECT) {
+			makeElementPop(square);
+		}
+		if (numSquares > MAX_SIZE_FOR_ROUND_BORDER) {
+			square.style.borderRadius = "";
+		}
+		squares.push(square);
+	}
+	squares.forEach((square) => {
+		const tracker = new BrightnessTracker(square);
+		brightnessTrackerMap.set(square, tracker);
+	});
+	addSquare(squares);
+}
+
+function makeElementPop(square) {
+	square.addEventListener("mouseover", (e) => {
+		e.target.classList.add("hovered");
+	});
+	square.addEventListener("mouseout", (e) => {
+		e.target.classList.remove("hovered");
+	});
+}
+
+function clearGrid() {
+	getAllSquares().forEach((square) => {
+		square.style.backgroundColor = gridColor;
+	});
+	brightnessTrackerMap.forEach((square) => {
+		square.resetBrightness();
+	});
+}
+
+function deleteGrid() {
+	grid.replaceChildren();
+}
+
+function makeNewGrid(newSize) {
+	deleteGrid();
+	createGrid(newSize);
+}
+
+function recolorGrid(color) {
+	gridColor = color;
+	clearGrid();
+}
+
+// #endregion
+
+// #region initialization functions
+
+function initResize() {
+	resizeButton.onclick = () => resizeModal.showModal();
+	resizeSlider.onmousemove = () => showSliderValue(resizeSlider.value);
+	resizeOk.onclick = () => makeNewGrid(resizeSlider.value);
+
+	function showSliderValue(value) {
+		rangeValue.textContent = value;
+	}
+}
+
+function initClear() {
+	clearButton.onclick = () => clearGrid();
+}
+
+function initRecolor() {
+	recolorButton.onclick = () => recolorModal.showModal();
+	recolorOk.onclick = () => recolorGrid(recolorPicker.value);
+}
+
+function initBrush() {
+	customizeBrushButton.onclick = () => brushModal.showModal();
+	brushColorMode.addEventListener("change", function () {
+		if (brushColorMode.value === "solid") {
+			brushColorPicker.style.display = "block";
+		} else {
+			brushColorPicker.style.display = "none";
+		}
+	});
+	brushOk.onclick = () => {
+		brushMode = brushColorMode.value
+			? brushColorMode.value
+			: DEFAULT_BRUSH_MODE;
+		brushColor = brushColorPicker.value;
+		darkeningEffect = darkEffectCheckbox.checked;
+	};
+}
+
+function initializeGrid() {
+	makeNewGrid(16);
+	initResize();
+	initClear();
+	initRecolor();
+	initBrush();
+}
+
+// #endregion
+
+window.onload = () => {
+	initializeGrid();
 };
